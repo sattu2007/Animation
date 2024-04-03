922a5665b22531e75c4ac8e72950b8feefefad56
Satyam Tiwari <118805002+sattu2007@users.noreply.github.com>
2024-04-03 10:53:18 +0530

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -8,7 +8,7 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(service=service, options=options)
-driver.get("https://github.com/sattu2007/Animation/")  # Update this with the path to your HTML file
+driver.get("https://sattu2007.github.io/Animation/")  # Update this with the path to your HTML file
 
 time.sleep(2)  # Adding a delay to see the result
 
